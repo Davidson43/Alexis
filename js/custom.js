7c11e1045282d9c3532fb2e502aa7c39b468691e
Davidson43 <nrrC@yandex.ru>
2022-04-05 08:34:57 +0300

    slider

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,26 @@
 $(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
+        margin:30,
         nav:false,
-        dots: true,
-        items:1
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive : {
+          0 : {
+            items:1
+          },
+          480 : {
+            items:2
+          },
+          768 : {
+            items:3
+          },
+          992 : {
+            items:4
+          }
+        }
     })
 
     $('.button-nav').on('click', function(event) {
